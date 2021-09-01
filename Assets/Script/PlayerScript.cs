93c75186d438f79f6c7037f5b24c26182d900807
Thinhnnn <thinhtm99@gmail.com>
2021-09-01 15:56:07 +0700

    update

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] Collider myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ignorePlayerCollision();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ignorePlayerCollision()
+    {
+        myCollider = GetComponent<Collider>();
+        //hàm này ngăn va chạm giữa các player
+        GameObject[] colliders = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject col in colliders)
+        {
+            Physics.IgnoreCollision(myCollider, col.GetComponent<Collider>());
+        }
+    }
+}
